0e235fe639caede47b0394b382022fe9e0cfe545
SWelbeck <swelbeck12@gmail.com>
2024-09-25 16:16:19 -0400

    added readme and new event listener for enter key

---
+++ README.md
@@ -0,0 +1,21 @@
+# SBA 316: The Document Object Model - Task List
+
+## Project Description
+
+This is a simple task list built using DOM manipulation.
+
+## Technologies Used
+
+- Javascript
+
+## How to get started
+
+Clone this repo to your computer and open the `index.html` file in a live server. This will land you on the home page where you can add tasks to the task list.
+
+## Future Updates
+
+- Add additional styling.
+
+## Acknowledgements
+
+- [W3Schools](https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp)
--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,10 @@
 // Adding a new task should create a row in the table
 // Delete task button/icon
 
-const tList = document.getElementById("taskList");
+// const tList = document.getElementById("taskList");
 const tInput = document.getElementById("taskInput");
-// const tBtn = document.getElementById("addTaskBtn");
-const tBtn = document.getElementById("submitBtn");
+const tBtn = document.querySelector("#addTaskBtn");
+// const tBtn = document.getElementById("submitBtn");
 const container = document.getElementById("toDoContainer");
 
 // Adding style elements
@@ -27,35 +27,43 @@ const cols = 2;
 const app = document.getElementById("taskTable");
 const table = document.createElement("table");
 const tableHead = document.createElement(`thead`);
+const headerRow = tableHead.insertRow(0);
+
+// Add Task Name heading
+const nameCell = headerRow.insertCell(0);
+nameCell.textContent = `Task Name`;
+console.log(nameCell.parentNode)
+
+// Add Due Date heading
+const dateCell = headerRow.insertCell(1);
+dateCell.textContent = `Due Date`;
+console.log(dateCell.parentElement)
 table.appendChild(tableHead);
 app.appendChild(table);
-// console.log(table);
-// console.log(table.rows);
 console.log(table);
-tableHead.textContent = `Task List`;
-// if (table.rows.length == 0 && c == 0) {
-//       td.textContent = "Task Name";
-//     } else if (table.rows.length == 0 && c == 1) {
-//       td.textContent = "Due Date";}
 
 // Event listener for Task button
 tBtn.addEventListener(`click`, handleClick);
-// tBtn.addEventListener(`submit`, handleClick);
-addEventListener(`keydown`, (handleClick) => {});
-onkeydown = (handleClick) => {};
+// Event listener for Enter key
+tInput.addEventListener(`keydown`, handleEnterBtn);
 
 // The function to handle adding new tasks.
-// function handleEnter(event){
+function handleEnterBtn(event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("addTaskBtn").click();
+  }
+}
 
-// }
-
-function handleClick(event) {
+function handleClick() {
   const newTask = tInput.value;
-
   // If task input is empty, don't do anything
   if (newTask === "") return;
 
-  //Creating a new row in the table
+  // Create a new row in the table
   const tr = document.createElement("tr");
   tr.style.borderBottom = `1px solid black`; // Not working at the moment
 
